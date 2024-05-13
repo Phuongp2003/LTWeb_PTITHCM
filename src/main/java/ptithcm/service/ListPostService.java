65f26e59ec1e73090d1014603311d79a9d1f59d5
Phuong <phambuinamphuong@gmail.com>
2024-05-13 14:33:09 +0700

    ?

--- src/main/java/ptithcm/service/ListPostService.java
+++ src/main/java/ptithcm/service/ListPostService.java
@@ -5,7 +5,6 @@ import java.util.List;
 import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
-import org.hibernate.Transaction;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
